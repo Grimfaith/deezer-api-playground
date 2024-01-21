7d3e86755a178e32c3763f6ef2b7d7900d3462db
= <pkaloulou@gmail.com>
2024-01-21 20:56:15 +0100

    Added a function that log to deezer

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import './style.css'
+import app_config from './app_config.env.json'
+
+// Redirect to Deezer's login page
+// to authenticate user
+function redirectToDeezerLogin() : void {
+    const authEndpoint = new URL(`${window.location}dz-login`);
+    authEndpoint.searchParams.set("app_id", app_config.deezer.app_id);
+    authEndpoint.searchParams.set("redirect_uri", window.location.origin);
+
+    window.location.replace(authEndpoint.toString());
+}
