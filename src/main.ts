d3d0a6ff7addb7c0c9f0bebe4ce6ee02d5fa6afd
= <pkaloulou@gmail.com>
2024-01-21 21:37:13 +0100

    linked login function to button

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import './style.css'
 import app_config from './app_config.env.json'
 
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.getElementsByClassName('dz-login')[0];
+    loginButton.addEventListener('click', redirectToDeezerLogin)
+})
+
 // Redirect to Deezer's login page
 // to authenticate user
 function redirectToDeezerLogin() : void {
