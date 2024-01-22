b58ab28df74264cdd3c6ed770adf852033db0844
= <pkaloulou@gmail.com>
2024-01-22 22:13:29 +0100

    added token and api endpoints

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,16 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     server: {
         proxy: {
-            '/dz-login': {
+            '/dz-login/auth': {
                 target: 'https://connect.deezer.com/oauth/auth.php',
+                changeOrigin: true
+            },
+            '/dz-login/token': {
+                target: 'https://connect.deezer.com/oauth/access_token.php',
+                changeOrigin: true
+            },
+            '/dz-api': {
+                target: 'https://api.deezer.com',
                 changeOrigin: true
             }
         }
