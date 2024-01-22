bc31168cfed118fbad40bfea7219480e0ca0c685
= <pkaloulou@gmail.com>
2024-01-22 15:34:58 +0100

    refactored login function so that it opens a new tab

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,21 @@ import './style.css'
 import app_config from './app_config.env.json'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const loginButton = document.getElementsByClassName('dz-login')[0];
-    loginButton.addEventListener('click', redirectToDeezerLogin)
+    const loginButton = document.querySelector<HTMLAnchorElement>('.dz-login')
+    if (loginButton) loginButton.addEventListener('click', openDeezerLoginTab)
 })
 
-// Redirect to Deezer's login page
-// to authenticate user
-function redirectToDeezerLogin() : void {
+// Opens a new tab allowing the user
+// to enable the application using their Deezer account
+let deezerLoginWindow: Window | null = null
+function openDeezerLoginTab() : void {
     const authEndpoint = new URL(`${window.location}dz-login`);
     authEndpoint.searchParams.set("app_id", app_config.deezer.app_id);
     authEndpoint.searchParams.set("redirect_uri", window.location.origin);
 
-    window.location.replace(authEndpoint.toString());
+    if (deezerLoginWindow === null || deezerLoginWindow.closed) {
+        deezerLoginWindow = window.open(authEndpoint.toString(), 'DeezerLoginWindow');
+    } else {
+        deezerLoginWindow.focus();
+    }
 }
