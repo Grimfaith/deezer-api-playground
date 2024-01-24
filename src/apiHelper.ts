ade61ec2948b559e45841dd72c088634dfa1d0d3
= <pkaloulou@gmail.com>
2024-01-24 18:37:38 +0100

    feat: added a function that return the user's flow

--- src/apiHelper.ts
+++ src/apiHelper.ts
@@ -34,7 +34,6 @@ export function openLoginWindow(loginWindow: Window | null) : Window | null {
  * @return {Promise<string|null>}
  */
 async function generateAccessToken(code: string) : Promise<string | null> {
-    let token = null
     const tokenEndpoint : URL = new URL(`${window.location.origin}/dz-login/token`)
     tokenEndpoint.searchParams.set("app_id", app_config.deezer.app_id)
     tokenEndpoint.searchParams.set("secret", app_config.deezer.app_secret_key)
@@ -44,12 +43,11 @@ async function generateAccessToken(code: string) : Promise<string | null> {
         const response : Response = await fetch(tokenEndpoint.toString())
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
         const data : string = await response.text()
-        token = data.split('&')[0].split('=')[1]
+        return  data.split('&')[0].split('=')[1]
     } catch (error) {
         console.error('Error fetching token : ', error)
+        return null
     }
-
-    return token
 }
 
 /**
@@ -58,20 +56,37 @@ async function generateAccessToken(code: string) : Promise<string | null> {
  * @returns {Promise<JSON|null>}
  */
 export async function getUserData(code: string) : Promise<IUserProfile | null> {
-    let userData = null
     let token : string | null = await generateAccessToken(code)
-    const userEndpoint : URL = new URL(`${window.location.origin}/dz-api/user`)
+    const userEndpoint : URL = new URL(`${window.location.origin}/dz-api/user/me`)
 
     if (token) {
         userEndpoint.searchParams.set("access_token", token)
         try {
             const response : Response = await fetch(userEndpoint.toString())
             if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
-            userData = await response.json()
+            return await response.json()
         } catch (error) {
             console.error('Error fetching user : ', error)
+            return null
         }
-    }
+    } else return null
+}
 
-    return userData
+/**
+ * Retrieves the user flow for the specified user ID
+ *
+ * @param {number} userID
+ * @return {Promise<object|null>}
+ */
+export async function getUserFlow(userID: number) : Promise<any> {
+    const flowEndpoint : URL = new URL(`${window.location.origin}/dz-api/user/${userID}/flow`)
+
+    try {
+        const response : Response = await fetch(flowEndpoint.toString())
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
+        return await response.json()
+    } catch (error) {
+        console.error('Error fetching user flow : ', error)
+        return null
+    }
 }
