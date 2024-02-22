38b12578a665929ecd0810d7298afbd7f27b2339
= <pkaloulou@gmail.com>
2024-02-22 23:11:05 +0100

    feat: added function that convert array to string

--- src/utils.ts
+++ src/utils.ts
@@ -8,3 +8,14 @@ export function checkQueryParams (key : string) : string | null {
     let params : URLSearchParams = new URLSearchParams(window.location.search)
     return params.get(key)
 }
+
+/**
+ * Converts a 2-dimensional array to a CSV string
+ *
+ * @param {string[][]} data
+ * @returns {string}
+ */
+export function arrayToCsv (data : string[][]) : string
+{
+    return data.map(row => row.join(',')).join('\n');
+}
